939ac039994baad93ae7ba96cd340e85c51589b2
varnikak00 <varnikak7070@gmail.com>
2021-12-04 10:59:03 +0530

    Listpractice

---
+++ listown practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SingleLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList obj = new SinglyLinkedList();
+
+            obj.AddinEmptyList(20);
+            obj.AddLast(30);
+            obj.AddBegining(10);
+            int x;
+            Console.WriteLine("Enter the postion ");
+            x = int.Parse(Console.ReadLine());
+            obj.AtPosition(90, 20);
+            obj.AddBefore(80, 90);
+            obj.DeleteFirst();
+            obj.removeLast();
+            Console.WriteLine("before Deletion");
+            obj.Display();
+            Console.WriteLine("after deletion");
+            obj.RemoveAnyNode(30);
+            obj.Display();
+
+            
+            
+        }
+    }
+}
---
+++ listown practice/SinglyLinkedList.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleLinkedList
+{
+    public class SinglyLinkedList
+    {
+        private Node Head;
+
+        public SinglyLinkedList()
+        {
+            this.Head = null;
+        }
+
+        public void AddinEmptyList(int data)
+        {
+            Node temp = new Node(data);
+            Head = temp;
+            Console.WriteLine("Data is successfully Added :" + Head.Info);
+        }
+        public void AddLast(int data)
+        {
+            Node p = Head;
+            Node temp = new Node(data);
+            if (Head == null)
+            {
+                Head = temp;
+                return;
+            }
+            while (p.link != null)
+            {
+                p = p.link;
+            }
+            p.link = temp;
+
+        }
+        public void AddBegining(int data)
+        {
+            Node temp = new Node(data);
+            temp.link = Head;
+            Head = temp;
+        }
+        public void Display()
+        {
+            Node p;
+            p = Head;
+            while (p != null)
+            {
+                Console.WriteLine(p.Info);
+                p = p.link;
+            }
+
+        }
+        public void AtPosition(int data, int x)
+        {
+            Node p = Head;
+            while (p != null)
+            {
+                if (p.Info == x)
+                    break;
+                p = p.link;
+            }
+
+            if (p == null)
+                Console.WriteLine("Node is Not Present");
+            else
+            {
+                Node temp = new Node(data);
+                temp.link = p.link;
+                p.link = temp;
+            }
+
+        }
+
+        public void AddBefore(int data, int x)
+        {
+            Node p = Head;
+            while (p != null)
+            {
+                if (p.link.Info == x)
+                    break;
+                p = p.link;
+            }
+            Node temp = new Node(data);
+            temp.link = p.link;
+            p.link = temp;
+        }
+        public void removeLast()
+        {
+            if (Head == null)
+                return;
+            if (Head.link == null)
+            {
+                Head = null;
+                return;
+            }
+            Node p = Head;
+            while (p.link.link != null)
+            {
+                p = p.link;
+            }
+            p.link = null;
+        }
+
+        public void RemoveAnyNode(int x)
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            if (Head.Info == x)
+            {
+                Head = Head.link;
+                return;
+            }
+
+            Node p = Head;
+            while (p.link != null)
+            {
+                if (p.link.Info == x)
+                    break;
+                p = p.link;
+            }
+            if (p.link == null)
+            {
+                Console.WriteLine("Node not in list");
+            }
+            else
+            {
+                p.link = p.link.link;
+            }
+        }
+
+        public void DeleteFirst()
+        {
+            if (Head == null)
+                return; ;
+            Head = Head.link;
+        }
+
+    }
+
+    public class Node
+    {
+        public int Info;
+        public Node link;
+
+        public Node(int data)
+        {
+            this.Info = data;
+            link = null;
+
+        }
+    }
+}
